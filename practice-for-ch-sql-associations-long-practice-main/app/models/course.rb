e85137b1a0395b78b5564148f77c464961b48821
Ethanjonm <ethanjonmercado@gmail.com>
2023-04-06 15:28:36 -0400

    added has_many for course.rb

--- practice-for-ch-sql-associations-long-practice-main/app/models/course.rb
+++ practice-for-ch-sql-associations-long-practice-main/app/models/course.rb
@@ -10,4 +10,17 @@
 #  updated_at    :datetime         not null
 #
 class Course < ApplicationRecord
+
+    has_many :enrollments,
+        class_name: :Enrollment,
+        primary_key: :id,
+        foreign_key: :student_id
+
+
+    has_many :enrolled_students,
+        class_name: :User,
+        primary_key: :id,
+        foreign_key: :id
+
+    
 end
--- practice-for-ch-sql-associations-long-practice-main/app/models/user.rb
+++ practice-for-ch-sql-associations-long-practice-main/app/models/user.rb
@@ -8,4 +8,13 @@
 #  updated_at :datetime         not null
 #
 class User < ApplicationRecord
+    has_many :enrollments,
+        class_name: :Enrollment,
+        primary_key: :id,
+        foreign_key: :student_id
+
+    has_many :enrolled_courses,
+        class_name: :Enrollment,
+        primary_key: :id,
+        foreign_key: :course_id
 end
